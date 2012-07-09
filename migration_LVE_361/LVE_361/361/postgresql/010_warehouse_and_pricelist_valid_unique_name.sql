37e8c4d6d40aa7dfac2d3487f62584f1b290b0ef
aparra <aparra@localhost>
2012-07-09 14:22:51 -0530

    Modificacion de validacion para Nombres de Almacenes y Precios de Listas. No se permitia repetir nombres globalmente, ahora se valida que no se repitan nombre para la misma organizacion.

---
+++ migration_LVE_361/LVE_361/361/postgresql/010_warehouse_and_pricelist_valid_unique_name.sql
@@ -0,0 +1,21 @@
+DROP INDEX m_warehouse_name;
+
+CREATE UNIQUE INDEX m_warehouse_name
+  ON m_warehouse
+  USING btree
+  (ad_client_id, ad_org_id, name);
+
+DROP INDEX m_pricelist_name;
+
+CREATE UNIQUE INDEX m_pricelist_name
+  ON m_pricelist
+  USING btree
+  (ad_client_id, ad_org_id, name);
+
+INSERT INTO LVE_SqlScript
+(LVE_SqlScript_ID,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,Date1,
+LastScript,ScriptUser) VALUES
+((select case when max(LVE_SqlScript_ID) is null then 1000000 else max(LVE_SqlScript_ID)+1 end from LVE_SqlScript),0,0,'Y',CURRENT_TIMESTAMP,0,CURRENT_TIMESTAMP,0,CURRENT_TIMESTAMP,
+'010_warehousename.sql',
+'Angel Parra')
+;
