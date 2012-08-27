6fd1873fb57e61e21a5ee0f6c3bc6212978fc146
rsalazar <rsalazar@localhost>
2012-08-27 10:20:15 -0530

    LVE_RequisitionToRFQ

--- extend/src/org/doubleclick/process/LVE_RequisitionToRfq.java
+++ extend/src/org/doubleclick/process/LVE_RequisitionToRfq.java
@@ -79,9 +79,13 @@ public class LVE_RequisitionToRfq extends SvrProcess
 			String name = para[i].getParameterName();
 			if (para[i].getParameter() == null)
 				;
+			else if (name.equals("AD_Org_ID"))
+				p_AD_Org_ID = para[i].getParameterAsInt();
+			else if (name.equals("AD_Client_ID"))
+				p_AD_Client_ID = para[i].getParameterAsInt();
 			else if (name.equals("M_Requisition_ID"))
 				p_M_Requisition_ID = para[i].getParameterAsInt();
-			else if (name.equals("C_Rfq_ID"))
+			else if (name.equals("C_RfQ_ID"))
 				p_C_Rfq_ID = para[i].getParameterAsInt();
 			
 			else
@@ -117,135 +121,23 @@ public class LVE_RequisitionToRfq extends SvrProcess
 				
 				while (rs.next()){
 					
-//					Copy Lines
+                    //Copy Lines
 					MRfQLine newLine = new MRfQLine ( mrfq);
-						newLine.setLine(rs.getInt(4));
-						newLine.setDescription(rs.getString(3));
-					//	newLine.setHelp(lines[i].getHelp());
-						newLine.setM_Product_ID(rs.getInt(1));
-					//	newLine.setM_AttributeSetInstance_ID(lines[i].getM_AttributeSetInstance_ID());
-					//	newLine.setDateWorkStart();
-					//	newLine.setDateWorkComplete();
-				//		newLine.setDeliveryDays(lines[i].getDeliveryDays());
-						newLine.saveEx();
-						//	Copy Qtys
-						
-							MRfQLineQty newQty = new MRfQLineQty (newLine);
-					//		newQty.setC_UOM_ID(qtys[j].getC_UOM_ID());
-							newQty.setQty(rs.getBigDecimal(2));
-							newQty.setIsOfferQty( true);
-					//		newQty.setIsPurchaseQty(qtys[j].isPurchaseQty());
-					//		newQty.setMargin(qtys[j].getMargin());
-							newQty.saveEx();
-						
+					newLine.setLine(rs.getInt(4));
+					newLine.setDescription(rs.getString(3));
+				    newLine.setM_Product_ID(rs.getInt(1));
+				    newLine.saveEx();
+					//	Copy Qtys
+					MRfQLineQty newQty = new MRfQLineQty (newLine);
+					newQty.setC_UOM_ID(100);
+					newQty.setQty(rs.getBigDecimal(2));
+					newQty.saveEx();				
 				
 			    }
 			   
 			    pstmt.close();
 			    return  "Proceso completado. "  ;
 		}
-	
-//	
-//			String sqlupd = ""
-//				+ "UPDATE c_invoice "
-//				+ "SET    ispaid='N' "
-//				+ "WHERE c_invoice_id in (" + invoices+ ")";
-//			
-//		    int no1 = DB.executeUpdate(sqlupd, null);
-//			log.fine("LVE_DeletePaymente -> c_invoice Update #" + no1);
-//			msg+= (no1<0) ?" Error... LVE_DeletePaymente." :"";
-//
-//			String sqldel= ""
-//							+ "DELETE FROM lco_invoicewithholding "
-//							+ "WHERE  lco_invoicewithholding_id IN (SELECT lco_invoicewithholding_id "
-//							+ "                                     FROM   lve_paymentwithholding "
-//							+ "                                     WHERE  c_payment_id = '"+p_C_Payment_ID+"') ";
-//
-//			 int no2 = DB.executeUpdate(sqldel, null);
-//			 log.fine("LVE_DeletePaymente -> lco_invoicewithholding Delete #" + no2);
-//			 msg+= (no2<0) ?" Error... LVE_DeletePaymente." :"";
-//
-//			 
-//			 String sqldelFacAcc= ""
-//					+ "DELETE FROM fact_acct "
-//					+ "WHERE   ad_table_id= '735' and record_id in ( "
-//					+ 							"SELECT c_allocationhdr_id "
-//					+ 							"FROM   c_allocationline "
-//					+ 							"WHERE  c_payment_id =  '"+p_C_Payment_ID+"') ";
-//
-//			 int no3 = DB.executeUpdate(sqldelFacAcc, null);
-//			 log.fine("LVE_DeletePaymente -> fact_acct Delete #" + no3);
-//			 msg+= (no3<0) ?" Error... LVE_DeletePaymente." :"";
-//
-//		
-//			 sqldelFacAcc = ""
-//				 + "DELETE FROM fact_acct "
-//				 + "WHERE  record_id IN (SELECT c_allocationhdr_id "
-//				 + "                     FROM   c_allocationline "
-//				 + "                     WHERE  c_payment_id = '"+p_C_Payment_ID+"') "
-//				 + "       AND ad_table_id = '735' ";
-//			 no3 = DB.executeUpdate(sqldelFacAcc, null);
-//			 log.fine("LVE_DeletePaymente -> fact_acct Delete #" + no3);
-//			 msg+= (no3<0) ?" Error... LVE_DeletePaymente." :"";
-//
-//			 
-//			 String sql3 = ""
-//				 + "   SELECT c_allocationhdr_id "
-//				 + "   FROM   c_allocationline "
-//				 + "    WHERE  c_payment_id = '"+p_C_Payment_ID+"'";
-//
-//				
-//			PreparedStatement pstmt3 = null;
-//			ResultSet rs3 = null;
-//			pstmt3 = DB.prepareStatement(sql3, null);
-//			rs3 = pstmt3.executeQuery();
-//			String allocationhdr="";
-//			while (rs3.next()){
-//				allocationhdr+= "'"+String.valueOf((rs3.getInt(1)))+"',";
-//		    }
-//			allocationhdr+="'0'";
-//		    pstmt3.close();
-//		 
-//			 sqldel = ""
-//				 + "DELETE FROM c_allocationline "
-//				 + "WHERE  c_payment_id = '"+p_C_Payment_ID+"' ";
-//			 no2 = DB.executeUpdate(sqldel, null);
-//			 log.fine("LVE_DeletePaymente -> c_allocationline Delete #" + no2);
-//			 msg+= (no2<0) ?" Error... LVE_DeletePaymente." :"";
-//
-//
-//			 sqldel = ""
-//				 + "DELETE FROM  c_allocationhdr "
-//				 + "WHERE  c_allocationhdr_id IN ("+allocationhdr+") ";
-//
-//			 no2 = DB.executeUpdate(sqldel, null);
-//			 log.fine("LVE_DeletePaymente -> c_allocationhdr Delete #" + no2);
-//			 msg+= (no2<0) ?" Error... LVE_DeletePaymente." :"";
-//
-//			 
-//			 sqldel = ""
-//				 + "DELETE FROM c_payment "
-//				 + "WHERE  c_payment_id = '"+p_C_Payment_ID+"' ";
-//			 no2 = DB.executeUpdate(sqldel, null);
-//			 log.fine("LVE_DeletePaymente -> c_payment Delete #" + no2);
-//			 msg+= (no2<0) ?" Error... LVE_DeletePaymente." :"";
-//
-//			 
-//				
-//			
-//
-//		}
-//		
-//		catch (Exception e)
-//		{
-//			log.log(Level.SEVERE, sql, e);
-//		}
-//		
-//		return (msg=="") ? "Proceso completado. " : msg ;
-//	
-//	}	
-	
-	
 		
 	
 }	
