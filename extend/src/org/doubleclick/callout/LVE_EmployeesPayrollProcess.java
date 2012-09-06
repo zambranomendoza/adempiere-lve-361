572cdf97451cc7efd5996e75f782461c147354a9
jrodriguez <jrodriguez@localhost>
2012-09-06 16:34:32 -0530

    Optimizaci?n de procesos de n?mina dentro de la localizaci?n venezuela,  incorporaci?n de nuevas ventanas asociadas a talento humano, y funcionalidades para el pago de n?mina y pago a terceros (IVSS, INCES, BANAVIH, PROVEEDORES DE CESTA TICKETS)

--- extend/src/org/doubleclick/callout/LVE_EmployeesPayrollProcess.java
+++ extend/src/org/doubleclick/callout/LVE_EmployeesPayrollProcess.java
@@ -1,4 +1,5 @@
 package org.doubleclick.callout;
+//package org.eevolution.callout;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,7 +12,7 @@ import org.compiere.util.DB;
 import javax.swing.JOptionPane;
 
 /**
- *	LVE_EmployesPayrollProcess
+ *	LVE_Employees_Payroll_Process
  *	
  * @author Jenny Cecilia Rodriguez A - jrodriguez@dcsla.com, Double Click Sistemas http://www.dcsla.com
  */
@@ -25,7 +26,7 @@ public class LVE_EmployeesPayrollProcess extends CalloutEngine{
 	
 	
 	
-	public String addEmployees(Properties ctx, int WindowNo, GridTab mTab, GridField mField, Object value) throws SQLException
+	public String add_employees(Properties ctx, int WindowNo, GridTab mTab, GridField mField, Object value) throws SQLException
 	{
 		if (isCalloutActive())
 			return "";
@@ -67,9 +68,10 @@ public class LVE_EmployeesPayrollProcess extends CalloutEngine{
 				"				THEN 1000000 " +
 				"				ELSE (SELECT MAX(hr_process_employee_id) FROM hr_process_employee) END) " +
 				"				+ROW_NUMBER() over (ORDER BY e.c_bpartner_id) " +
-				"	FROM hr_employee e INNER JOIN hr_process p	" +
+				"	FROM c_bpartner c INNER JOIN hr_employee e ON c.c_bpartner_ID = e.c_bpartner_ID INNER JOIN hr_process p	" +
 				"		ON e.hr_payroll_id = p.hr_payroll_id AND p.hr_process_id = " + mTab.getValue("hr_process_id") + 
-				"	WHERE  (e.hr_department_id = " + mTab.getValue("hr_department_id") + 
+				"	    INNER JOIN hr_period pe on p.hr_period_id = pe.hr_period_id " +
+				"   WHERE  (e.hr_department_id = " + mTab.getValue("hr_department_id") + 
 									        " OR " + mTab.getValue("hr_department_id") + " IS NULL) " +
 				"	AND (e.hr_job_id = " +  mTab.getValue("hr_job_id") + 
 								   " OR " + mTab.getValue("hr_job_id") + " IS NULL) " +
@@ -78,12 +80,17 @@ public class LVE_EmployeesPayrollProcess extends CalloutEngine{
 				"     OR e.c_bpartner_ID = " +  mTab.getValue("c_bpartner_ID_F2_ID") +
 				"     OR (" + mTab.getValue("c_bpartner_ID") + " IS NULL AND " + mTab.getValue("c_bpartner_ID_F1_ID") + " IS NULL AND " + mTab.getValue("c_bpartner_ID_F2_ID") + " IS NULL)) " +
 				"	AND e.c_bpartner_ID NOT IN " +
-				"		(SELECT c_bpartner_ID FROM hr_process_employee WHERE hr_process_id = " + mTab.getValue("hr_process_id") + "))  "; 
+				"		(SELECT c_bpartner_ID FROM hr_process_employee WHERE hr_process_id = " + mTab.getValue("hr_process_id") + ") " +
+				"   AND c.isactive = 'Y' AND e.isactive = 'Y' AND c.isemployee = 'Y' " +
+				"   AND pe.startdate >= e.startdate " +
+				"	AND e.ad_client_id = " +  mTab.getValue("ad_client_id") + 
+				"	AND e.ad_org_id = " +  mTab.getValue("ad_org_id") + ")"; 
 	
 			int no2 = DB.executeUpdate(sql, null);		   
 	
 			if (no2 > 0)
 			{
+				JOptionPane.showMessageDialog(null, "Los empleados fueron creados con éxito", "ALERTA", JOptionPane.INFORMATION_MESSAGE);
 				return "Los empleados fueron creados con éxito";	
 			}
 			else
